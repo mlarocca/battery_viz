597c21da2f3e6fb8aaf5dcb01ba477c844b3ad9e
Marcello La Rocca <marcellolarocca@gmail.com>
2015-11-24 10:06:37 +0000

    WiFi

--- public/js/demo.js
+++ public/js/demo.js
@@ -4,13 +4,13 @@ requirejs.config({
       "jquery": "jquery-2.0.3.min",
       "ractive": "ractive",
       "fade": "ractive-transitions-fade",
-      "bootstrap": "bootstrap.min"    
-    },      
+      "bootstrap": "bootstrap.min"
+    },
     "shim": {
         "bootstrap": {
             deps: ['jquery'],
             exports: 'bootstrap'
-        },  
+        },
         "ractive": {
             exports: 'Ractive'
         }
@@ -27,19 +27,26 @@ require([
   /** @module MeterViz
     * Includes all the front-end functionalities for the MeterViz demo
     * It uses Ractive to achieve a better MCV separation; therefore, it needs a Ractive (html) template to properly work.
-    * Note: by using Require.js to define the AMD module, there is no global scope poisoning, 
+    * Note: by using Require.js to define the AMD module, there is no global scope poisoning,
     * and all the variables and methods stays private and safe.
     **/
 
-  var BATTERY_SERVICE_URL =  "http://localhost:8080/battery",       //"/public/json/battery.json"
-      BATTERY_CHECK_INTERVAL = 120000,    //Check battery status every 2 minutes.
+  var BATTERY_SERVICE_URL =  "http://localhost:8080/battery",
+      WIFI_SERVICE_URL = "http://localhost:8080/networks",
+      BATTERY_CHECK_INTERVAL = 120000,          //Check battery status every 2 minutes.
       BATTERY_RED_THRESHOLD = 15,               //Percentage at which the battery goes to 'red' zone
       BATTERY_YELLOW_THRESHOLD = 55,            //Percentage at which the battery enters 'yellow' zone
       BATTERY_RED_CLASS = 'battery-red',
       BATTERY_YELLOW_CLASS = 'battery-yellow',
       BATTERY_GREEN_CLASS = 'battery-green',
       BATTERY_STATE_DISCHARGING = 'discharging',
-      batteryUpdateTimerId = null,      //ID of the timer for battery update (so it can be paused)    
+
+      WIFI_CHECK_INTERVAL = 5000,       //Check wifi networks every 5 secs.
+      WIFI_PROTECTED_KEY = 'Encryption key',
+      WIFI_PROTECTION_ON = 'on',
+
+      batteryUpdateTimerId = null,      //ID of the timer for battery update (so it can be paused)
+      networksUpdateTimerId = null,     //ID of the timer for the wifi update function
 
       ractive = new Ractive({
         el: 'panels',
@@ -57,12 +64,10 @@ require([
           networksUpdateError: false,   //True <=> the update daemon for the wifi info has reported an error at its last try
           selectedNetworkItem: null,    //From the list of networks, which one has been selected? (DOM element)
           selectedNetwork: null,        //From the list of networks, which one has been selected? (JSON item)
-          
-
 
                               /** @method batteryStateClass
                                *  @for MeterViz.Ractive
-                               *  
+                               *
                                *  Return the appropriate class for the text showing the state of the battery.
                                *  @param {String} state   The current state of the battery.
                                *  @return {String} The most appropriate CSS class for the text.
@@ -72,13 +77,23 @@ require([
           },
                               /** @method batteryLifeClass
                                *  @for MeterViz.Ractive
-                               *  
+                               *
                                *  Return the appropriate class for the text showing the remaining time until the battery goes empty.
                                *  @param {Number} percent   The current capacity (in percentage).
                                *  @return {String} The most appropriate CSS class for the text.
-                               */          
+                               */
           batteryLifeClass: function (percent) {
                               return percent <= BATTERY_RED_THRESHOLD ? BATTERY_RED_CLASS : (percent <= BATTERY_YELLOW_THRESHOLD ? BATTERY_YELLOW_CLASS : BATTERY_GREEN_CLASS);
+          },
+                              /** @method isNetworkEncrypted
+                               *  @for LiteDesk.Ractive
+                               *
+                               *  Return true iff the network passed as parameter is protected with encryption.
+                               *  @param {Object} network   The current cell item.
+                               *  @return {Boolean} true iff the network is protected.
+                               */
+          isNetworkEncrypted: function (network) {
+            return network[WIFI_PROTECTED_KEY] === WIFI_PROTECTION_ON;
           }
         }
       });
@@ -89,7 +104,7 @@ require([
                     *
                     * In response to a click event, it pauses the daemon that looks for updates on the battery status.
                     * @return {undefined}
-                    */                                
+                    */
                   "battery-pause":  function () {
                                       clearInterval(batteryUpdateTimerId);
                                       ractive.set('batteryPaused', true);
@@ -101,23 +116,66 @@ require([
                     *
                     * In response to a click event, it restarts the daemon that looks for updates on the battery status.
                     * @return {undefined}
-                    */      
+                    */
                   "battery-play":   function () {
                                       updateBatteryStatus();  //Checks the status immediately, then starts the daemon
                                       batteryUpdateTimerId = setInterval(updateBatteryStatus, BATTERY_CHECK_INTERVAL);
                                       ractive.set('batteryPaused', false);
-                                    }                                                               
+                                    },
+                  /** @method networks-pause
+                    * @for MeterViz.Ractive
+                    *
+                    * In response to a click event, it pauses the daemon that looks for updates on the list of available networks.
+                    * @return {undefined}
+                    */
+                  "networks-pause": function () {
+                                      clearInterval(networksUpdateTimerId);
+                                      ractive.set('networksPaused', true);
+                                    },
+
+                  /** @method networks-play
+                    * @for MeterViz.Ractive
+                    *
+                    * In response to a click event, it restarts the daemon that looks for updates on the list of available networks.
+                    * @return {undefined}
+                    */
+                  "networks-play":  function () {
+                                      updateWifiNetworksList();     //Checks the status immediately, then starts the daemon
+                                      networksUpdateTimerId = setInterval(updateWifiNetworksList, WIFI_CHECK_INTERVAL);
+                                      ractive.set('networksPaused', false);
+                                    },
+
+                  /** @method expandWiFi
+                    * @for LiteDesk.Ractive
+                    *
+                    * Perform a series of actions to give more info about the wifi network entry selected from the list.
+                    * @param {Object} event   The Ractive wrapper for the event that triggered the function
+                    * @return {undefined}
+                    */
+                  expandWifi:   function (event) {
+                                    var selectedNetworkItem = ractive.get('selectedNetworkItem'),
+                                        currentSelection = $(event.node);
+                                    if (selectedNetworkItem && $.isFunction(selectedNetworkItem.toggleClass)) {
+                                      selectedNetworkItem.toggleClass("active");
+                                    }
+                                    currentSelection.toggleClass("active");
+                                    ractive.set('selectedNetworkItem', currentSelection);
+                                    ractive.set('selectedNetwork', event.context);
+                                }
                 });
 
-  
+
   /** @method updateBatteryStatus
     * @for MeterViz
-    * 
+    *
     * Starts an AJAX call to asynchronously call the server that provides info about the battery.
     * @return {undefined}
     */
   function updateBatteryStatus () {
-    $.getJSON(BATTERY_SERVICE_URL)
+    $.ajax(BATTERY_SERVICE_URL, {
+        dataType: 'json',
+        jsonp: false
+      })
       .then(function (battery) {
         ractive.set('batteryUpdateError', false);
         //WARNING: this is the only place where fields from the original feed are processed.
@@ -135,11 +193,32 @@ require([
       });
   }
 
+  /** @method updateBatteryStatus
+    * @for LiteDesk
+    *
+    * Starts an AJAX call to asynchronously call the server that provides info about the network status.
+    * @return {undefined}
+    */
+  function updateWifiNetworksList () {
+    $.ajax(WIFI_SERVICE_URL, {
+        dataType: 'json',
+        jsonp: false
+      })
+      .then(function (networks) {
+        ractive.set('networksUpdateError', false);
+        ractive.set('wifiNetworks', networks);
+      }).fail(function () {
+        ractive.set('networksUpdateError', true);
+      });
+  }
+
   //Start the daemons that will check the battery and networks status...
   batteryUpdateTimerId = setInterval(updateBatteryStatus, BATTERY_CHECK_INTERVAL);
+  networksUpdateTimerId = setInterval(updateWifiNetworksList, WIFI_CHECK_INTERVAL);
 
   //... but also updates the status of battery and network immediately
   updateBatteryStatus();
+  updateWifiNetworksList();
 
   //Adds a tooltip for the play/pause buttons, and the net-error icon
   $("[data-toggle='tooltip']").tooltip({container: $('body')});
--- server.js
+++ server.js
@@ -8,6 +8,14 @@ var BASE_URL = './';
 //Regular expressions for battery and networks paths. We are building a RESTful API, so URL's path will matter
 //For battery info, the acceptable patterns are '/battery' and all the ones starting with '/battery/'
 var RE_BATTERY = /\/battery\/?/;
+//For wifi info, the acceptable patterns are '/network', '/networks', '/wifi', and all the ones starting with either '/network/', '/networks/' or '/wifi/'
+var RE_NETWORKS = /\/(?:(?:networks?)|(?:wifi))\/?/;
+
+var NET_CELL_PREFIX = 'Cell';
+var NET_ADDRESS_PREFIX = 'Address';
+var NET_QUALITY_PREFIX = 'Quality';
+var NET_SIGNAL_PREFIX = 'Signal level';
+var NET_EXTRA_PREFIX = 'Extra';
 
 var CONFIG = switchConfigForCurrentOS();
 
@@ -16,34 +24,43 @@ var WINDOWS_CHARGING_STATE_MAP = {
   2: 'charging'
 };
 
-var BATTERY_ERROR_MESSAGE = '404 - Unable to retrieve battery status';
+var BATTERY_ERROR_MESSAGE = '500 - Unable to retrieve battery status';
+var WIFI_ERROR_MESSAGE = '500 - Unable to retrieve wifi status';
 
 function switchConfigForCurrentOS () {
   switch(process.platform) {
-    case 'linux': 
+    case 'linux':
       return {
-      	command: 'upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|time to empty|to full|percentage"',
-      	processFunction: processBatteryStdoutForLinux
+      	batteryCommand: 'upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|time to empty|to full|percentage"',
+        batteryProcessFunction: processBatteryStdoutForLinux,
+        wifiCommand: 'iwlist wlan0 scanning | egrep "Cell |Address|Channel|Frequency|Encryption|Quality|Signal level|Last beacon|Mode|Group Cipher|Pairwise Ciphers|Authentication Suites|ESSID"',
+        wifiProcessFunction: processWifiStdoutForLinux
       };
     case 'darwin': //MAC
       return {
-      	command: 'pmset -g batt | egrep "([0-9]+\%).*" -o',
-      	processFunction: processBatteryStdoutForMac
-      }; 
+      	batteryCommand: 'pmset -g batt | egrep "([0-9]+\%).*" -o',
+      	batteryProcessFunction: processBatteryStdoutForMac,
+        wifiCommand: '',
+        wifiProcessFunction: function () {}
+      };
     case 'win32':
       return {
-      	command: 'WMIC Path Win32_Battery',
-      	processFunction: processBatteryStdoutForWindows
+      	batteryCommand: 'WMIC Path Win32_Battery',
+      	batteryProcessFunction: processBatteryStdoutForWindows,
+        wifiCommand: '',
+        wifiProcessFunction: function () {}
       };
     default:
       return {
-      	command: '',
-      	processFunction: function () {}
+      	batteryCommand: '',
+      	batteryProcessFunction: function () {},
+        wifiCommand: '',
+        wifiProcessFunction: function () {}
       };
   }
 }
 
-function processLineForLinux(battery, line) {
+function processBatteryLineForLinux(battery, line) {
   var key;
   var val;
 
@@ -60,10 +77,7 @@ function processLineForLinux(battery, line) {
   return battery;
 }
 
-function processLineForWindows(battery, key, val) {
-  var key;
-  var val;
-
+function processBatteryLineForWindows(battery, key, val) {
   key = key.trim();
   val = val.trim();
   battery[key] = val;
@@ -71,7 +85,7 @@ function processLineForWindows(battery, key, val) {
   return battery;
 }
 
-function mapKeysForLinux(battery) {
+function mapBatteryKeysForLinux(battery) {
 	var mappedBattery = {};
 	mappedBattery.percentage = battery.percentage;
 	mappedBattery.state = battery.state;
@@ -79,7 +93,7 @@ function mapKeysForLinux(battery) {
 	return mappedBattery;
 }
 
-function mapKeysForMac(battery) {
+function mapBatteryKeysForMac(battery) {
 	var mappedBattery = {};
 	mappedBattery.percentage = battery[0];
 	mappedBattery.state = battery[1];
@@ -87,7 +101,7 @@ function mapKeysForMac(battery) {
 	return mappedBattery;
 }
 
-function mapKeysForWindows(battery) {
+function mapBatteryKeysForWindows(battery) {
   var mappedBattery = {};
   mappedBattery.percentage = battery['EstimatedChargeRemaining'];
   mappedBattery.state = WINDOWS_CHARGING_STATE_MAP[battery['BatteryStatus']];
@@ -97,20 +111,20 @@ function mapKeysForWindows(battery) {
 
 function processBatteryStdoutForLinux(stdout) {
 	var battery = {},
-			processLine = processLineForLinux.bind(null, battery);
+			processLine = processBatteryLineForLinux.bind(null, battery);
   stdout.split('\n').forEach(processLine);
-  return mapKeysForLinux(battery);
+  return mapBatteryKeysForLinux(battery);
 }
 
 function processBatteryStdoutForMac(stdout) {
 	var battery = stdout.split(';').map(trimParam);
-  return mapKeysForMac(battery);
+  return mapBatteryKeysForMac(battery);
 }
 
 function processBatteryStdoutForWindows(stdout) {
   var lines = stdout.split('\n').map(trimParam),
       battery = {},
-      processLine = processLineForWindows.bind(null, battery),
+      processLine = processBatteryLineForWindows.bind(null, battery),
       headersStr,
       paramsStr,
       headers,
@@ -118,7 +132,7 @@ function processBatteryStdoutForWindows(stdout) {
       lastIndex,
       i,
       n;
-  
+
   if (lines.length < 2) {
     return {};
   }
@@ -128,7 +142,7 @@ function processBatteryStdoutForWindows(stdout) {
   headers = headersStr
     .split(' ')
     .filter(function(s) {
-      return s.length > 0; 
+      return s.length > 0;
     });
   lastIndex = -1;
   fieldsPositions = headers
@@ -142,10 +156,104 @@ function processBatteryStdoutForWindows(stdout) {
     processLine(headers[i], paramsStr.substr(fieldsPositions[i], fieldsPositions[i+1] - fieldsPositions[i]));
   }
 
-  return mapKeysForWindows(battery);
+  return mapBatteryKeysForWindows(battery);
 }
 
-function onBatteryInfo(response, data) {
+function stringStartsWith (string, prefix) {
+    return string.slice(0, prefix.length) == prefix;
+}
+
+/**
+  * @method processWifiLineForLinux
+  * @description
+  * Extracts the parameter(s) in a line of the input file (as generated by iwlist wlan0 scanning)
+  * and adds it (them) to the cell's dictionary.
+  *
+  * @param {String} line The line of text from the input file to be scanned.
+  * @param {Object} cell The object gathering current cell's properties.
+  *
+  * @return {Object} A dictionary with key-value pairs.
+  */
+function processWifiLineForLinux(cell, line) {
+  var key;
+  var val;
+
+  line = line.trim();
+  if (line.length > 0) {
+    switch (true) {
+
+      case stringStartsWith(line, NET_ADDRESS_PREFIX):
+      line = line.split(':');
+      line.splice(0, 1);
+      //INVARIANT: Address in the format Address: DC:0B:1A:47:BA:07
+      if (line.length > 0) {
+        cell[NET_ADDRESS_PREFIX] = line.join(":");
+      }
+      break;
+    case stringStartsWith(line, NET_QUALITY_PREFIX):
+      //INVARIANT: this line must have a similar format: Quality=41/70  Signal level=-69 dBm
+      line = line.split(NET_SIGNAL_PREFIX);
+      cell[NET_QUALITY_PREFIX] = line[0].split("=")[1].trim();
+      if (line.length > 1) {
+        cell[NET_SIGNAL_PREFIX] = line[1].split("=")[1].trim();
+      }
+      break;
+    case stringStartsWith(line, NET_EXTRA_PREFIX):
+      //INVARIANT: this line must have a similar format: Extra: Last beacon: 1020ms ago
+      line = line.split(":");
+      //we can ignore the prefix of the string
+      if (line.length > 2) {
+        cell[line[1].trim()] = line[2].trim();
+      }
+      break;
+    default:
+      //INVARIANT: the field must be formatted as "key : value"
+      line = line.split(":");
+      if (line.length > 1) {
+        //Just stores the key-value association, so that coupling with client is reduced to the min:
+        //values will be examined only on the client side
+        cell[line[0].trim()] = line[1].trim();
+      }
+    }
+  }
+
+  return cell;
+}
+
+function mapWifiKeysForLinux(d) {
+  return d;
+}
+
+function processWifiStdoutForLinux(stdout) {
+  var networks = {};
+  var net_cell = "";
+  var cell = {};
+
+  stdout.split('\n').map(trimParam).forEach(function (line) {
+    if (line.length > 0) {
+      //check if the line starts a new cell
+      if (stringStartsWith(line, NET_CELL_PREFIX)) {
+        if (net_cell.length > 0) {
+          networks[net_cell] = mapWifiKeysForLinux(cell);
+        }
+        cell = {};
+        line = line.split("-");
+        net_cell = line[0].trim();
+        line = line[1];
+      }
+      //Either way, now we are sure we have a non empty line with (at least one) key-value pair
+      //       and that cell has been properly initialized
+      processWifiLineForLinux(cell, line);
+    }
+
+  });
+  if (net_cell.length > 0) {
+    networks[net_cell] = mapWifiKeysForLinux(cell);
+  }
+  return networks;
+}
+
+function jsonResponseWrapper(response, data) {
   response.writeHead(200, {
     'Content-Type': 'application/json',
     'Content-Length': data.length,
@@ -154,6 +262,9 @@ function onBatteryInfo(response, data) {
   response.write(data);
   response.end();
 }
+
+var onBatteryInfo = jsonResponseWrapper;
+var onWifiInfo = jsonResponseWrapper;
 
 function onError(response, msg) {
   response.writeHead(404, {'Content-Type': 'text/plain'});
@@ -167,16 +278,16 @@ function trimParam(param) {
 
 function getBatteryStatus(response, onSuccess, onError) {
 
-  child_process.exec(CONFIG.command, function execBatteryCommand(err, stdout, stderr) {
+  child_process.exec(CONFIG.batteryCommand, function execBatteryCommand(err, stdout, stderr) {
     var battery;
-    
+
     if (err) {
       console.log('child_process failed with error code: ' + err.code);
       onError(response, BATTERY_ERROR_MESSAGE);
     } else {
 
 			try {
-	      battery = CONFIG.processFunction(stdout);
+	      battery = CONFIG.batteryProcessFunction(stdout);
 	      onSuccess(response, JSON.stringify(battery));
 	    } catch (e) {
 	      console.log(e);
@@ -184,6 +295,27 @@ function getBatteryStatus(response, onSuccess, onError) {
 	    }
 	  }
 	});
+}
+
+function getWifiStatus(response, onSuccess, onError) {
+
+  child_process.exec(CONFIG.wifiCommand, function execWifiCommand(err, stdout, stderr) {
+    var wifi;
+
+    if (err) {
+      console.log('child_process failed with error code: ' + err.code);
+      onError(response, WIFI_ERROR_MESSAGE);
+    } else {
+
+      try {
+        wifi = CONFIG.wifiProcessFunction(stdout);
+        onSuccess(response, JSON.stringify(wifi));
+      } catch (e) {
+        console.log(e);
+        onError(response, WIFI_ERROR_MESSAGE);
+      }
+    }
+  });
 }
 
 var server = http.createServer(function (request, response) {
@@ -198,7 +330,9 @@ var server = http.createServer(function (request, response) {
     response.end();
   } else if (RE_BATTERY.test(requestUrl)) {
     getBatteryStatus(response, onBatteryInfo, onError);
-  } else {  
+  } else if (RE_NETWORKS.test(requestUrl)) {
+    getWifiStatus(response, onWifiInfo, onError);
+  } else {
     fs.exists(filePath, function (exists) {
 
       if (exists) {
